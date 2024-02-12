aa289e0d83bcd39a0ecbd59ed709de1292f5cdda
Sato <katsuryoku2@gmail.com>
2024-02-12 00:03:34 -0600

    start editor

--- src/components/midPanel.vue
+++ src/components/midPanel.vue
@@ -1,15 +1,41 @@
 <template>
-
-    <div class="midPanelInner">
-        <h1>Mid Component</h1>
-    </div>
-    
+  <div class="midPanelInner">
+    <h1>Mid Component</h1>
+    <div class="editor" ref="editorContainer"></div>
+  </div>
 </template>
 
 <style>
-    .midPanelInner {
-        background-color: purple;
-        height: 100%;
-        width: 100%;
-    }
+.midPanelInner {
+  background-color: purple;
+
+  height: 100%;
+  width: 100%;
+
+}
+.editor {
+    height: 800px;
+}
 </style>
+
+<script>
+import * as monaco from "monaco-editor";
+
+export default {
+  name: 'OctdatEditor',
+  mounted() {
+    this.editor = monaco.editor.create(this.$refs.editorContainer, {
+      value: 'function spicyKatsu() {\n\tconsole.log("Hello octdat");\n}',
+      language: 'octdat',
+      theme: 'vs-dark',
+    });
+  },
+  beforeUnmount() {
+    if (this.editor) {
+      this.editor.dispose();
+    }
+  }
+}
+
+
+</script>
--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import MonacoEditorPlugin from 'vite-plugin-monaco-editor';
 
 // https://vitejs.dev/config
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    MonacoEditorPlugin({
+      // Specify options here
+      languageWorkers: ['editorWorkerService', 'octdat']
+    })
+  ]
 });
