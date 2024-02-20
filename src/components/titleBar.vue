0c91aea989e1ee620374a997e3e20ccd3f16ffb2
KNelTech <k.nelsontech@gmail.com>
2024-02-19 23:43:06 -0600

    added menu to title bar

--- src/components/titleBar.vue
+++ src/components/titleBar.vue
@@ -1,5 +1,63 @@
 <template>
   <div id="titleBar">
+    <div class="d-flex justify-space-around">
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn
+          color="primary"
+          v-bind="props"
+        >
+          Activator slot
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          :value="index"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <v-btn
+      color="primary"
+    >
+      Parent activator
+
+      <v-menu activator="parent">
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            :value="index"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-btn>
+
+    <v-btn
+      id="menu-activator"
+      color="primary"
+    >
+      Sibling activator
+    </v-btn>
+
+    <v-menu activator="#menu-activator">
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          :value="index"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
     <div class="title">
       <span id="titleText">{{ title }}</span>
       <img id="logo" :src="logoSrc" alt="NFModTool" />
@@ -22,6 +80,17 @@
 <script>
 export default {
   name: 'TitleBar',
+  data() {
+    return {
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' },
+      ],
+      // ... other data properties
+    };
+  },
   props: {
     title: {
       type: String,
