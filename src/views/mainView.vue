f5ef4c643fd7e09ba9ff8d7ad21892e91accfc08
KNelTech <k.nelsontech@gmail.com>
2024-03-14 22:06:10 -0500

    first steps into something really cool.

--- src/views/mainView.vue
+++ src/views/mainView.vue
@@ -1,6 +1,39 @@
 <template>
-  <div id="appMain">
-    <h4>HEY!</h4>
-  </div>
+      <div id="appMain">
+       <button @click="increment">Count up: {{ counterUp }}</button>
+       <button @click="decrement">Count down: {{ counterDown }}</button> 
+      </div>
 </template>
-0
+    
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const counterUp = ref('');
+const counterDown = ref('');
+
+onMounted(() => {
+  counterUp.value = 0;
+  counterDown.value = 100;
+});
+
+function increment() {
+    counterUp.value++;
+  }
+
+  function decrement() {
+    counterDown.value--;
+  }
+
+</script>
+
+
+<style>
+button {
+    background-color: purple !important;
+    border-style: none;
+ 	  color: white;
+  	margin: 25px;
+    height: 35px;
+    border-radius: 12px;
+}
+</style>
